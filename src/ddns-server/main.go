9523ba54ea7bd9ad97412287bc752cbf019b0a89
Ricardo Cermeño <ricardocermenoab@gmail.com>
2024-11-11 23:44:22 -0500

    get ip to a proof of concept

--- src/ddns-server/main.go
+++ src/ddns-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	// "log"
 	// "os"
@@ -15,14 +16,23 @@ func handleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (ev
 
 	fmt.Println("___Init___")
 	fmt.Println(event.Headers)
+
+	ip := ""
+
+	for key, value := range event.Headers {
+		if key == "X-Forwarded-For" {
+			ip = strings.Split(value, ",")[0]
+		}
+	}
+	fmt.Println(ip)
 	fmt.Println(event.Body)
 	fmt.Println("___End___")
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello Word",
+		Body:       "good " + ip,
 		Headers: map[string]string{
-			"Content-type": "application/json",
+			"Content-type": "text/plain",
 		},
 	}, nil
 }
